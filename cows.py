a32f3b57a96419b45eba01b02970a5727102d1ce
Chester Liu <chesterl@internal.synopsys.com>
2018-10-01 21:57:56 -0400

    first version

---
+++ cows.py
@@ -0,0 +1,8 @@
+for c in range(1,100):
+    for p in range(1,100):
+        for k in range(1,100):
+            animals = c + p + k
+            if animals == 100:
+                price = c*10 + p*3 + k/2
+                if price == 100:
+                    print("cows=", c, " pigs=", p, "chickens=", k)
